1dadfe08fc783cd4ee4a3c8dff611023f83baf4e
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-18 15:19:05 +0900

    feat: CI/CD 수정

--- src/main/java/com/eventorback/global/config/DataSourceConfig.java
+++ src/main/java/com/eventorback/global/config/DataSourceConfig.java
@@ -1,34 +1,34 @@
-// package com.eventorback.global.config;
-//
-// import org.apache.commons.dbcp2.BasicDataSource;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-//
-// @Configuration
-// public class DataSourceConfig {
-//
-// 	@Value("${spring.datasource.url}")
-// 	private String url;
-//
-// 	@Bean
-// 	public BasicDataSource dataSource() {
-// 		BasicDataSource dataSource = new BasicDataSource();
-// 		dataSource.setUrl(url);
-// 		dataSource.setUsername("root");
-// 		dataSource.setPassword("1q2w3e4r!");
-// 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//
-// 		// 최적화 파라미터 설정
-// 		dataSource.setInitialSize(100);
-// 		dataSource.setMaxTotal(100);
-// 		dataSource.setMaxIdle(100);
-// 		dataSource.setMinIdle(100);
-//
-// 		// 추가 최적화 설정
-// 		dataSource.setTestOnBorrow(true);
-// 		dataSource.setValidationQuery("SELECT 1");
-//
-// 		return dataSource;
-// 	}
-// }
+package com.eventorback.global.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUrl(url);
+		dataSource.setUsername("root");
+		dataSource.setPassword("1q2w3e4r!");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+		// 최적화 파라미터 설정
+		dataSource.setInitialSize(100);
+		dataSource.setMaxTotal(100);
+		dataSource.setMaxIdle(100);
+		dataSource.setMinIdle(100);
+
+		// 추가 최적화 설정
+		dataSource.setTestOnBorrow(true);
+		dataSource.setValidationQuery("SELECT 1");
+
+		return dataSource;
+	}
+}
