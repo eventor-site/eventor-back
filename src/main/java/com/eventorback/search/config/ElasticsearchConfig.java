99afeedc25215ec80f745d35a85e4cd9a1c2dc6e
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-18 23:08:56 +0900

    feat: 배포환경 ElasticsearchConfig 수정

--- src/main/java/com/eventorback/search/config/ElasticsearchConfig.java
+++ src/main/java/com/eventorback/search/config/ElasticsearchConfig.java
@@ -1,20 +1,7 @@
 package com.eventorback.search.config;
 
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
 import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -41,71 +28,71 @@ public class ElasticsearchConfig {
 		return new ElasticsearchClient(transport);
 	}
 
-	@Profile("prod")
-	@Bean
-	public static ElasticsearchClient prodCreateElasticsearchClient() {
-		try {
-			// ObjectMapper 설정
-			ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-
-			// 자격 증명 제공자 설정 (사용자 이름과 비밀번호)
-			BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-			// 각 호스트에 대해 동일한 자격증명을 사용할 경우, 아래와 같이 설정합니다.
-			credentialsProvider.setCredentials(
-				new AuthScope("elasticsearch-es01-1", 9200),
-				new UsernamePasswordCredentials("elastic", "elastic")
-			);
-			// 필요에 따라 다른 호스트도 추가할 수 있지만, 기본적으로 첫 번째 호스트에 설정된 자격증명이 적용됩니다.
-
-			// 모든 SSL 인증서를 신뢰하는 TrustManager 생성 (보안 위험: 테스트용으로만 사용)
-			TrustManager[] trustAllCerts = new TrustManager[] {
-				new X509TrustManager() {
-					@Override
-					public void checkClientTrusted(X509Certificate[] chain, String authType) {
-						// 아무 것도 하지 않음
-					}
-
-					@Override
-					public void checkServerTrusted(X509Certificate[] chain, String authType) {
-						// 아무 것도 하지 않음
-					}
-
-					@Override
-					public X509Certificate[] getAcceptedIssuers() {
-						return new X509Certificate[0];
-					}
-				}
-			};
-
-			// SSLContext 생성 및 초기화 (모든 인증서를 신뢰)
-			SSLContext sslContext = SSLContext.getInstance("TLS");
-			sslContext.init(null, trustAllCerts, new SecureRandom());
-
-			// RestClientBuilder에 SSLContext와 NoopHostnameVerifier, 그리고 자격증명 제공자를 설정
-			RestClientBuilder restClientBuilder = RestClient.builder(
-				new HttpHost("elasticsearch-es01-1", 9200, "https"),
-				new HttpHost("elasticsearch-es02-1", 9200, "https"),
-				new HttpHost("elasticsearch-es03-1", 9200, "https")
-			).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-				@Override
-				public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-					return httpClientBuilder
-						.setSSLContext(sslContext)
-						.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-						.setDefaultCredentialsProvider(credentialsProvider);
-				}
-			});
-
-			// RestClient 및 ElasticsearchTransport 설정
-			RestClient restClient = restClientBuilder.build();
-			ElasticsearchTransport transport = new RestClientTransport(restClient,
-				new JacksonJsonpMapper(objectMapper));
-
-			// ElasticsearchClient 생성 및 반환
-			return new ElasticsearchClient(transport);
-
-		} catch (Exception e) {
-			throw new RuntimeException("Elasticsearch 클라이언트 생성 실패", e);
-		}
-	}
+	// @Profile("prod")
+	// @Bean
+	// public static ElasticsearchClient prodCreateElasticsearchClient() {
+	// 	try {
+	// 		// ObjectMapper 설정
+	// 		ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+	//
+	// 		// 자격 증명 제공자 설정 (사용자 이름과 비밀번호)
+	// 		BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+	// 		// 각 호스트에 대해 동일한 자격증명을 사용할 경우, 아래와 같이 설정합니다.
+	// 		credentialsProvider.setCredentials(
+	// 			new AuthScope("elasticsearch-es01-1", 9200),
+	// 			new UsernamePasswordCredentials("elastic", "elastic")
+	// 		);
+	// 		// 필요에 따라 다른 호스트도 추가할 수 있지만, 기본적으로 첫 번째 호스트에 설정된 자격증명이 적용됩니다.
+	//
+	// 		// 모든 SSL 인증서를 신뢰하는 TrustManager 생성 (보안 위험: 테스트용으로만 사용)
+	// 		TrustManager[] trustAllCerts = new TrustManager[] {
+	// 			new X509TrustManager() {
+	// 				@Override
+	// 				public void checkClientTrusted(X509Certificate[] chain, String authType) {
+	// 					// 아무 것도 하지 않음
+	// 				}
+	//
+	// 				@Override
+	// 				public void checkServerTrusted(X509Certificate[] chain, String authType) {
+	// 					// 아무 것도 하지 않음
+	// 				}
+	//
+	// 				@Override
+	// 				public X509Certificate[] getAcceptedIssuers() {
+	// 					return new X509Certificate[0];
+	// 				}
+	// 			}
+	// 		};
+	//
+	// 		// SSLContext 생성 및 초기화 (모든 인증서를 신뢰)
+	// 		SSLContext sslContext = SSLContext.getInstance("TLS");
+	// 		sslContext.init(null, trustAllCerts, new SecureRandom());
+	//
+	// 		// RestClientBuilder에 SSLContext와 NoopHostnameVerifier, 그리고 자격증명 제공자를 설정
+	// 		RestClientBuilder restClientBuilder = RestClient.builder(
+	// 			new HttpHost("elasticsearch-es01-1", 9200, "https"),
+	// 			new HttpHost("elasticsearch-es02-1", 9200, "https"),
+	// 			new HttpHost("elasticsearch-es03-1", 9200, "https")
+	// 		).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+	// 			@Override
+	// 			public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+	// 				return httpClientBuilder
+	// 					.setSSLContext(sslContext)
+	// 					.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+	// 					.setDefaultCredentialsProvider(credentialsProvider);
+	// 			}
+	// 		});
+	//
+	// 		// RestClient 및 ElasticsearchTransport 설정
+	// 		RestClient restClient = restClientBuilder.build();
+	// 		ElasticsearchTransport transport = new RestClientTransport(restClient,
+	// 			new JacksonJsonpMapper(objectMapper));
+	//
+	// 		// ElasticsearchClient 생성 및 반환
+	// 		return new ElasticsearchClient(transport);
+	//
+	// 	} catch (Exception e) {
+	// 		throw new RuntimeException("Elasticsearch 클라이언트 생성 실패", e);
+	// 	}
+	// }
 }
