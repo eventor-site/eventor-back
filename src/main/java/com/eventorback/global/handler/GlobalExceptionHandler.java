0f2b5f4de0f1ba821832c843bb2cf96b079c9343
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-26 00:53:39 +0900

    feat: 서버 에러 확인중

--- src/main/java/com/eventorback/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/eventorback/global/handler/GlobalExceptionHandler.java
@@ -1,16 +1,16 @@
-// package com.eventorback.global.handler;
-//
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-// import com.eventorback.global.exception.GlobalException;
-//
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
-//
-// 	@ExceptionHandler(GlobalException.class)
-// 	public ResponseEntity<String> handleAccessDeniedException(GlobalException e) {
-// 		return ResponseEntity.status(e.getErrorStatus().getStatus()).body(e.getMessage());
-// 	}
-// }
+package com.eventorback.global.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.eventorback.global.exception.GlobalException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(GlobalException.class)
+	public ResponseEntity<String> handleAccessDeniedException(GlobalException e) {
+		return ResponseEntity.status(e.getErrorStatus().getStatus()).body(e.getMessage());
+	}
+}
