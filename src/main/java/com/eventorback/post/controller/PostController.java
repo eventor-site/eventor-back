9183fae9da46e58d4fe6213fe99748e7c56d290c
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-17 19:29:33 +0900

    feat: 관리자 기간별 이벤트 작성 게시물 조회 날짜 형식 수정

--- src/main/java/com/eventorback/post/controller/PostController.java
+++ src/main/java/com/eventorback/post/controller/PostController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -167,7 +168,8 @@ public class PostController {
 	@AuthorizeRole("admin")
 	@GetMapping("/statistic/users/admin")
 	ResponseEntity<ApiResponse<List<GetEventPostCountByAdminResponse>>> getEventPostCountByAdmin(
-		@RequestParam LocalDateTime startTime, @RequestParam LocalDateTime endTime) {
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime) {
 		return ApiResponse.createSuccess(postService.getEventPostCountByAdmin(startTime, endTime));
 	}
 }
