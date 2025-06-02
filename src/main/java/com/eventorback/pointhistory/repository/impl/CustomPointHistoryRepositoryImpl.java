4e6cfca4bb8891cad212a7247ea52a5c4b303daa
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-06-02 15:49:23 +0900

    feat: 포인트기록 페이징 처리 수정

--- src/main/java/com/eventorback/pointhistory/repository/impl/CustomPointHistoryRepositoryImpl.java
+++ src/main/java/com/eventorback/pointhistory/repository/impl/CustomPointHistoryRepositoryImpl.java
@@ -5,7 +5,6 @@ import static com.eventorback.pointhistory.domain.entity.QPointHistory.*;
 import static com.eventorback.user.domain.entity.QUser.*;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -26,23 +25,6 @@ public class CustomPointHistoryRepositoryImpl implements CustomPointHistoryRepos
 	@Override
 	public Page<GetUserPointTotalResponse> getUserPointTotalsByPeriod(LocalDateTime startTime, LocalDateTime endTime,
 		Pageable pageable) {
-		// 1. 페이징할 유저 ID 리스트 먼저 조회
-		List<Long> pagedUserIds = queryFactory
-			.select(pointHistory.user.userId)
-			.from(pointHistory)
-			.where(pointHistory.createdAt.between(startTime, endTime))
-			.groupBy(pointHistory.user.userId)
-			.orderBy(point.amount.sum().desc())
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
-			.fetch();
-
-		// 2. 유저 ID 리스트가 비어있을 수 있으므로 체크
-		if (pagedUserIds.isEmpty()) {
-			return new PageImpl<>(Collections.emptyList(), pageable, 0);
-		}
-
-		// 3. 해당 유저들의 상세 정보 및 합계 포인트 조회
 		List<GetUserPointTotalResponse> result = queryFactory
 			.select(Projections.constructor(
 				GetUserPointTotalResponse.class,
@@ -54,13 +36,14 @@ public class CustomPointHistoryRepositoryImpl implements CustomPointHistoryRepos
 			.from(pointHistory)
 			.join(pointHistory.point, point)
 			.join(pointHistory.user, user)
-			.where(pointHistory.user.userId.in(pagedUserIds),
-				pointHistory.createdAt.between(startTime, endTime))
+			.where(pointHistory.createdAt.between(startTime, endTime))
 			.groupBy(pointHistory.user.userId, pointHistory.user.nickname, pointHistory.user.email)
 			.orderBy(point.amount.sum().desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
 			.fetch();
 
-		// 4. 전체 count는 그대로 유지
+		// 전체 유저 수 (group 기준 개수)
 		Long total = queryFactory
 			.select(pointHistory.user.userId.countDistinct())
 			.from(pointHistory)
@@ -68,7 +51,6 @@ public class CustomPointHistoryRepositoryImpl implements CustomPointHistoryRepos
 			.fetchOne();
 
 		return new PageImpl<>(result, pageable, total != null ? total : 0L);
-
 	}
 
 }
