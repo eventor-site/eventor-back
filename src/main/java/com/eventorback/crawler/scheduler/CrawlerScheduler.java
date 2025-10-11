137c1208ec3c94012ecc625c01c383eb62dd23b1
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-10-11 10:58:01 +0900

    feat: 크롤링 중단

--- src/main/java/com/eventorback/crawler/scheduler/CrawlerScheduler.java
+++ src/main/java/com/eventorback/crawler/scheduler/CrawlerScheduler.java
@@ -1,27 +1,28 @@
-package com.eventorback.crawler.scheduler;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import com.eventorback.crawler.service.CrawlerService;
-import com.eventorback.global.config.LeaderElectionManager;
-
-import lombok.RequiredArgsConstructor;
-
+// package com.eventorback.crawler.scheduler;
+//
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
+//
+// import com.eventorback.crawler.service.CrawlerService;
+// import com.eventorback.global.config.LeaderElectionManager;
+//
+// import lombok.RequiredArgsConstructor;
+//
 // @Profile("prod")
-@Component
-@RequiredArgsConstructor
-public class CrawlerScheduler {
-	private final LeaderElectionManager leaderElectionManager;
-	private final CrawlerService crawlerService;
-
-	@Scheduled(cron = "0 */30 * * * *")
-	public void crawlHotDeals() {
-
-		if (!leaderElectionManager.tryAcquireLeadership()) {
-			return;
-		}
-
-		crawlerService.refreshAndSaveNewItems();
-	}
-}
+// @Component
+// @RequiredArgsConstructor
+// public class CrawlerScheduler {
+// 	private final LeaderElectionManager leaderElectionManager;
+// 	private final CrawlerService crawlerService;
+//
+// 	@Scheduled(cron = "0 */30 * * * *")
+// 	public void crawlHotDeals() {
+//
+// 		if (!leaderElectionManager.tryAcquireLeadership()) {
+// 			return;
+// 		}
+//
+// 		crawlerService.refreshAndSaveNewItems();
+// 	}
+// }
