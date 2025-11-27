7b0c47876ef8affba5f19e5d180fef7c5dd0fc8a
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-11-27 22:46:30 +0900

    Revert "feat: WebDriver 관련 제거"
    This reverts commit f2cafed024b6b6be96b6d660ea343b50b2c8bedd.

--- src/main/java/com/eventorback/crawler/util/WebDriverFactory.java
+++ src/main/java/com/eventorback/crawler/util/WebDriverFactory.java
@@ -1,29 +1,29 @@
-// package com.eventorback.crawler.util;
-//
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-//
-// public class WebDriverFactory {
-//
-// 	public static AutoCloseableWebDriver createChromeDriver() {
-// 		ChromeOptions options = new ChromeOptions();
-// 		options.addArguments("--headless=new");
-// 		options.addArguments("--no-sandbox");
-// 		options.addArguments("--disable-gpu");
-// 		options.addArguments("--disable-dev-shm-usage");
-// 		options.addArguments("--disable-extensions");
-// 		options.addArguments("--disable-background-timer-throttling");
-// 		options.addArguments("--disable-backgrounding-occluded-windows");
-// 		options.addArguments("--disable-renderer-backgrounding");
-// 		options.addArguments("--disable-features=TranslateUI");
-// 		options.addArguments("--disable-ipc-flooding-protection");
-// 		options.addArguments("--force-device-scale-factor=1");
-// 		options.addArguments("--window-size=1920,1080");
-// 		options.setExperimentalOption("useAutomationExtension", false);
-// 		options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-//
-// 		WebDriver driver = new ChromeDriver(options);
-// 		return new AutoCloseableWebDriver(driver);
-// 	}
-// }
+package com.eventorback.crawler.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverFactory {
+
+	public static AutoCloseableWebDriver createChromeDriver() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-background-timer-throttling");
+		options.addArguments("--disable-backgrounding-occluded-windows");
+		options.addArguments("--disable-renderer-backgrounding");
+		options.addArguments("--disable-features=TranslateUI");
+		options.addArguments("--disable-ipc-flooding-protection");
+		options.addArguments("--force-device-scale-factor=1");
+		options.addArguments("--window-size=1920,1080");
+		options.setExperimentalOption("useAutomationExtension", false);
+		options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+
+		WebDriver driver = new ChromeDriver(options);
+		return new AutoCloseableWebDriver(driver);
+	}
+}
--- src/main/java/com/eventorback/global/config/WebDriverConfig.java
+++ src/main/java/com/eventorback/global/config/WebDriverConfig.java
@@ -1,21 +1,21 @@
-// package com.eventorback.global.config;
-//
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-//
-// import io.github.bonigarcia.wdm.WebDriverManager;
-//
-// @Configuration
-// public class WebDriverConfig {
-//
-// 	@Bean(destroyMethod = "quit")
-// 	public WebDriver webDriver() {
-// 		WebDriverManager.chromedriver().setup();
-// 		ChromeOptions options = new ChromeOptions();
-// 		options.addArguments("--headless", "--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage");
-// 		return new ChromeDriver(options);
-// 	}
-// }
+package com.eventorback.global.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Configuration
+public class WebDriverConfig {
+
+	@Bean(destroyMethod = "quit")
+	public WebDriver webDriver() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage");
+		return new ChromeDriver(options);
+	}
+}
